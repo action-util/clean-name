5ba2a3ba4a944949b63fe9f73fe7d62dd2666a37
Jesse A. Tov <jesse.tov@gmail.com>
2020-02-15 20:35:51 -0600

    Cleanup.

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const github = require('@actions/github');
 try {
   const dirty = core.getInput('dirty');
   console.log(`dirty: “${dirty}”`);
+  
   const clean = dirty.replace(/[\\\/':<>|*?]/g, '_');
-  core.setOutput("clean", clean);
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  console.log(`clean: “${clean}”`);
+  core.setOutput('clean', clean);
 } catch (error) {
   core.setFailed(error.message);
 }
